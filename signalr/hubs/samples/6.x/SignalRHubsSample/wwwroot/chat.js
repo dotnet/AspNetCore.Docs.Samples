20034eafccff40ca364bce23d6b4d1ddeaccf18b
Kirk Larkin <6025110+serpent5@users.noreply.github.com>
2022-03-28 14:55:50 +0100

    SignalR Hubs v6.0

---
+++ signalr/hubs/samples/6.x/SignalRHubsSample/wwwroot/chat.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chat")
+    .build();
+
+  connection.on("ReceiveMessage", (user, message) => {
+    const li = document.createElement("li");
+    li.textContent = `${user}: ${message}`;
+    document.getElementById("messageList").appendChild(li);
+  });
+
+  document.getElementById("send").addEventListener("click", async () => {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+
+    // <snippet_TryCatch>
+    try {
+      await connection.invoke("SendMessage", user, message);
+    } catch (err) {
+      console.error(err);
+    }
+    // </snippet_TryCatch>
+  });
+
+  async function start() {
+    try {
+      await connection.start();
+      console.log("SignalR Connected.");
+    } catch (err) {
+      console.log(err);
+      setTimeout(start, 5000);
+    }
+  };
+
+  connection.onclose(async () => {
+    await start();
+  });
+
+  // Start the connection.
+  start();
+});
