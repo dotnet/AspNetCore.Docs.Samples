25c92daa1a3bc3e010aae3c57f8d7089491f8256
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-07-14 12:47:50 -1000

    Tryparse (#5)
    * Add TryParse samples
    
    * Add TryParse samples

---
+++ mvc/models/BindTryParseAPI/Models/Culture.cs
@@ -0,0 +1,29 @@
+namespace BindTryParseAPI.Models
+{
+    // <snippet>
+    public class Culture
+    {
+        public string? DisplayName { get; }
+
+        public Culture(string displayName)
+        {
+            if (string.IsNullOrEmpty(displayName))
+                throw new ArgumentNullException(nameof(displayName));
+
+            DisplayName = displayName;
+        }
+
+        public static bool TryParse(string? value, out Culture? result)
+        {
+            if (value is null)
+            {
+                result = default;
+                return false;
+            }
+
+            result = new Culture(value);
+            return true;
+        }
+    }
+    // </snippet>
+}
