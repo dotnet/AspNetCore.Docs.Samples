93e60a26f7306afaa5c7e7d0dd360e51ada81bc6
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-08-26 10:38:39 +0100

    edited file

--- fundamentals/middleware/problem-detail-service/Program.cs
+++ fundamentals/middleware/problem-detail-service/Program.cs
@@ -18,29 +18,82 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// middleware to handle writing problem details to the response
+app.Use(async(context, next)=>{
+    // added the MathErrorFeature to the request pipeline
+    context.Features.Set(new MathErrorFeature());
 
-app.MapGet("/weatherforecast", () =>
+    await next(context);
+
+    if (context.RequestServices.GetService<IProblemDetailsService>() is { } problemDetailsService)
+    {
+        MathErrorType matherror = context.Features.Get<MathErrorFeature>()!.MathError;
+        if (matherror == MathErrorType.DivisionByZeroError)
+        {
+            await problemDetailsService.WriteAsync(new ProblemDetailsContext
+            {
+                HttpContext = context,
+                ProblemDetails = new()
+                {
+                    Title = "Wrong Input",
+                    Detail = "The number you inputed is zero",
+                    Type = "https://en.wikipedia.org/wiki/Division_by_zero"
+                }
+            });
+        }
+        if (matherror == MathErrorType.NegativeRadicandError)
+        {
+            await problemDetailsService.WriteAsync(new ProblemDetailsContext
+            {
+                HttpContext = context,
+                ProblemDetails = new()
+                {
+                    Title = "Wrong Input",
+                    Detail = "Negative or complex numbers are not handled",
+                    Type = "https://en.wikipedia.org/wiki/Square_root"
+                }
+            });
+        }
+    }
+});
+
+// endpoint for dividing numbers
+app.MapGet("/divide", async (HttpContext context, double numerator, double denominator) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    if (denominator == 0)
+    {
+        context.Features.Get<MathErrorFeature>()!.MathError = MathErrorType.DivisionByZeroError;
+        return Results.BadRequest();
+    }
+
+    var calculation = await Task.FromResult(numerator / denominator);
+    return Results.Ok(calculation);
+});
+
+// endpoint for obtaining the squareroot of a number
+app.MapGet("/squareroot", async (HttpContext context, int radicand) =>
+{
+    if (radicand < 0)
+    {
+        context.Features.Get<MathErrorFeature>()!.MathError = MathErrorType.NegativeRadicandError;
+        return Results.BadRequest();
+    }
+
+    var calculation = await Task.FromResult(Math.Sqrt(radicand));
+    return Results.Ok(calculation);
+});
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// Custom math errors
+enum MathErrorType
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    DivisionByZeroError,
+    NegativeRadicandError
+}
+
+// Custom Http Request Feature
+class MathErrorFeature
+{
+    public MathErrorType MathError { get; set; }
 }
