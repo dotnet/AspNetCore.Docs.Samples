05aa9ab95279a7c721e146166a3215c087604b27
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-09-21 22:32:33 +0100

    Problemdetailsmiddleware (#63)
    * added middleware feature
    
    * removed statuscodepages
    
    * formatted code
    
    * Removed unnecessary middleware 😅
    
    * setErrorType from handler
    
    * setErrorType from handlers
    
    * deleted the has path func
    
    * fixed for middleware
    
    * setting for controllers
    
    * fixed iss for controllers 😎
    
    * formatted code 😎
    
    * Removed unnecessary middleware
    
    * edit
    
    * added third controller
    
    * added back code
    
    * code reset

--- fundamentals/middleware/problem-details-service/Controllers/ValuesController.cs
+++ fundamentals/middleware/problem-details-service/Controllers/ValuesController.cs
@@ -78,3 +78,59 @@ public class ValuesController : ControllerBase
 
 }
 // </snippet>
+
+// <snippet>
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class Values3Controller : ControllerBase
+{
+    // /api/values/divide/1/2
+    [HttpGet("{Numerator}/{Denominator}")]
+    public IActionResult Divide(double Numerator, double Denominator)
+    {
+        if (Denominator == 0)
+        {
+            var errorType = new MathErrorFeature
+            {
+                MathError =
+                                                   MathErrorType.DivisionByZeroError
+            };
+            HttpContext.Features.Set(errorType);
+            return Problem(
+                title: "Wrong Input",
+                detail: "The number you inputed is zero",
+                type: "https://en.wikipedia.org/wiki/Division_by_zero",
+                statusCode: StatusCodes.Status400BadRequest
+                );
+        }
+
+        var calculation = Numerator / Denominator;
+        return Ok(calculation);
+    }
+
+    // /api/values/squareroot/4
+    [HttpGet("{radicand}")]
+    public IActionResult Squareroot(double radicand)
+    {
+        if (radicand < 0)
+        {
+            var errorType = new MathErrorFeature
+            {
+                MathError =
+                                                   MathErrorType.NegativeRadicandError
+            };
+            HttpContext.Features.Set(errorType);
+            return Problem(
+                title: "Wrong Input",
+                detail: "Negative or complex numbers are not handled",
+                type: "https://en.wikipedia.org/wiki/Square_root",
+                statusCode: StatusCodes.Status400BadRequest
+                );
+        }
+
+        var calculation = Math.Sqrt(radicand);
+        return Ok(calculation);
+    }
+
+}
+// </snippet>
