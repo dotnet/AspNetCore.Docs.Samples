3a838f4c8100008df3c64116ecc844bd9a885a56
Tom Dykstra <tdykstra@microsoft.com>
2022-09-30 14:37:55 -0700

    Change names and add snippet tags

--- fundamentals/minimal-apis/samples/MultipleResultTypes/Program.cs
+++ fundamentals/minimal-apis/samples/MultipleResultTypes/Program.cs
@@ -7,11 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<List<Book>>((sp) =>
 {
     return new(){
-    new Book(1, "Testing Dot", "John Doe"),
-    new Book(2, "Learn Linq", "Rick Perry"),
-    new Book(3, "Generics", "Dalis Chevy"),
-    new Book(4, "Testing the Mic", "Bob Tik"),
-    new Book(5, "Drop the Dot", "Farmy Lix"),
+    new Book(1, "Testing Dot", "Carson Alexander"),
+    new Book(2, "Learn Linq", "Meredith Alonso"),
+    new Book(3, "Generics", "Arturo Anand"),
+    new Book(4, "Testing the Mic", "Gytis Barzdukas"),
+    new Book(5, "Drop the Dot", "Van Li"),
 };
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,14 +29,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// <snippet_multiple_result_types>
 app.MapGet("/book{id}", Results<Ok<Book>, NotFound> (int id, List<Book> bookList) =>
 {
     return bookList.FirstOrDefault((i) => i.Id == id) is Book book
      ? TypedResults.Ok(book)
      : TypedResults.NotFound();
 });
+// </snippet_multiple_result_types>
 
+// <snippet_single_result_type>
 app.MapGet("/books", (List<Book> bookList) => TypedResults.Ok(bookList));
+// </snippet_single_result_type>
 
 app.Run();
+
 record Book(int Id, string Title, string Author);
