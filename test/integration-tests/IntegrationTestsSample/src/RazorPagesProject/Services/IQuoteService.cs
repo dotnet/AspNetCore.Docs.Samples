5aac3b4b9774277bbeebf7a1fdfc05aba8aa9675
Jos van der Til <jvandertil@users.noreply.github.com>
2022-10-05 02:16:58 +0200

    Integration test sample for .NET 7 (#84)
    * Initial import of 3.x projects
    
    * Update packages to .NET 7.0
    
    * Remove WebApi client dependency
    
    * Give authentication scheme a more explicit name
    
    * Remove BuildServiceProvider calls
    
    * Set environment explicitly in web app factory
    
    * Move to WebApplicationBuilder
    
    * Move Program to top level statements
    
    * Add solution file
    
    * Convert to file scoped namespaces
    
    Co-authored-by: Jos van der Til <jvdtil@quintor.nl>

---
+++ test/integration-tests/IntegrationTestsSample/src/RazorPagesProject/Services/IQuoteService.cs
@@ -0,0 +1,10 @@
+using System.Threading.Tasks;
+
+namespace RazorPagesProject.Services;
+
+#region snippet1
+public interface IQuoteService
+{
+    Task<string> GenerateQuote();
+}
+#endregion
---
+++ test/integration-tests/IntegrationTestsSample/tests/RazorPagesProject.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesProject.Data;
+
+namespace RazorPagesProject.Tests;
+
+#region snippet1
+public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<ApplicationDbContext>));
+
+            services.Remove(descriptor);
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
+#endregion
