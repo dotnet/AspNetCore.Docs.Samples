c2a30074651e0335f32ca900584be41fca3806b9
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2022-10-14 14:18:37 -1000

    Add more rate limiter samples (#101)
    * Add more rate limiter samples
    
    * Add more rate limiter samples

--- fundamentals/middleware/rate-limit/WebRate2/Models/MyRateLimitOptions.cs
+++ fundamentals/middleware/rate-limit/WebRate2/Models/MyRateLimitOptions.cs
@@ -3,10 +3,11 @@ namespace WebRateLimitAuth.Models;
 public class MyRateLimitOptions
 {
     public const string MyRateLimit = "MyRateLimit";
-    public int PermitLimit { get; set; } = 100;
+    public int PermitLimit { get; set; } = 10;
+    public int SlidingPermitLimit { get; set; } = 100;
     public int Window { get; set; } = 10;
     public int ReplenishmentPeriod { get; set; } = 2;
-    public int QueueLimit { get; set; } = 2;
+    public int QueueLimit { get; set; } = 100;
     public int SegmentsPerWindow { get; set; } = 8;
     public int TokenLimit { get; set; } = 10;
     public int TokenLimit2 { get; set; } = 20;
---
+++ fundamentals/middleware/rate-limit/WebRate2/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace WebRate2.Pages;
+// <snippet_1>
+[EnableRateLimitingAttribute("sliding")]
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+// </snippet_1>
