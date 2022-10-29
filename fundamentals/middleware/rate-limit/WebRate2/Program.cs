660aa233ad53502cdda7b48a439e305d3ceb22da
Fiyaz Bin Hasan <fiyazhasan@live.com>
2022-10-29 18:27:15 +0600

    Update Program.cs

--- fundamentals/middleware/rate-limit/WebRate2/Program.cs
+++ fundamentals/middleware/rate-limit/WebRate2/Program.cs
@@ -113,4 +113,64 @@ app.MapDefaultControllerRoute();
 
 app.Run();
 // </snippet_2>
+#elif CHAINED
+// <snippet_3>
+using System.Globalization;
+using System.Threading.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRateLimiter(_ =>
+{
+    _.OnRejected = (context, _) =>
+    {
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            context.HttpContext.Response.Headers.RetryAfter =
+                ((int) retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+        }
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.");
+
+        return new ValueTask();
+    };
+    _.GlobalLimiter = PartitionedRateLimiter.CreateChained(
+        PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        {
+            var userAgent = httpContext.Request.Headers.UserAgent.ToString();
+
+            return RateLimitPartition.GetFixedWindowLimiter
+            (userAgent, _ =>
+                new FixedWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 4,
+                    Window = TimeSpan.FromSeconds(2)
+                });
+        }),
+        PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        {
+            var userAgent = httpContext.Request.Headers.UserAgent.ToString();
+            
+            return RateLimitPartition.GetFixedWindowLimiter
+            (userAgent, _ =>
+                new FixedWindowRateLimiterOptions
+                {
+                    AutoReplenishment = true,
+                    PermitLimit = 20,    
+                    Window = TimeSpan.FromSeconds(30)
+                });
+        }));
+});
+
+var app = builder.Build();
+app.UseRateLimiter();
+
+static string GetTicks() => (DateTime.Now.Ticks & 0x11111).ToString("00000");
+
+app.MapGet("/", () => Results.Ok($"Hello {GetTicks()}"));
+
+app.Run();
+// </snippet_3>
 #endif
