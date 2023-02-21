8e5c0072e6fca932b48e05bedd008ff7e6277890
Günther Foidl <gue@korporal.at>
2023-02-21 17:02:23 +0100

    Added project

---
+++ fundamentals/host/TimedBackgroundTasks/Program.cs
@@ -0,0 +1,10 @@
+using TimedBackgroundTasks;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
