87fac0e5d43e46465ccc9b6adc0a0487db370667
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-02-28 19:05:17 +0100

    added check for shortname (#126)

--- mvc/models/ModelStateError/Models/Contact.cs
+++ mvc/models/ModelStateError/Models/Contact.cs
@@ -5,6 +5,7 @@ public class Contact
     public int Id { get; set; }
 
     public string Name { get; set; } = String.Empty;
+    public string ShortName { get; set; } = String.Empty;
     public string Email { get; set; } = String.Empty;
     public string PhoneNumber { get; set; } = String.Empty;
 }
