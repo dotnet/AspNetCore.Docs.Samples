8839b50e6ae20b2fcd55182725c7283e359a9824
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-02-28 22:45:50 +0100

    Model state error2 (#127)
    * added check for shortname
    
    * removed some files
    
    * little edit to shortname check
    
    * added shortname
    
    * added comment
    
    * changes
    
    * added shortname 2
    
    * added binding for shortname

--- mvc/models/ModelStateError/Controllers/ContactsController.cs
+++ mvc/models/ModelStateError/Controllers/ContactsController.cs
@@ -1,163 +1,192 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ModelStateError.Data;
 using ModelStateError.Models;
 
-namespace ModelStateError.Controllers
+namespace ModelStateError.Controllers;
+
+public class ContactsController : Controller
 {
-    public class ContactsController : Controller
+    private readonly ModelStateErrorContext _context;
+
+    public ContactsController(ModelStateErrorContext context)
     {
-        private readonly ModelStateErrorContext _context;
+        _context = context;
+    }
 
-        public ContactsController(ModelStateErrorContext context)
+    // GET: Contacts
+    public async Task<IActionResult> Index()
+    {
+        return _context.Contact != null ?
+                    View(await _context.Contact.ToListAsync()) :
+                    Problem("Entity set 'ModelStateErrorContext.Contact'  is null.");
+    }
+
+    // GET: Contacts/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null || _context.Contact == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Contacts
-        public async Task<IActionResult> Index()
+        var contact = await _context.Contact
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (contact == null)
         {
-              return _context.Contact != null ? 
-                          View(await _context.Contact.ToListAsync()) :
-                          Problem("Entity set 'ModelStateErrorContext.Contact'  is null.");
+            return NotFound();
         }
 
-        // GET: Contacts/Details/5
-        public async Task<IActionResult> Details(int? id)
+        return View(contact);
+    }
+
+    // GET: Contacts/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Contacts/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    // <snippet_4>
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Name,ShortName,Email,PhoneNumber")] Contact contact)
+    {
+        // Attach Validation Error Message to the Model on validation failure.
+
+        if (_context.Contact.Any(i => i.PhoneNumber == contact.PhoneNumber))
         {
-            if (id == null || _context.Contact == null)
-            {
-                return NotFound();
-            }
-
-            var contact = await _context.Contact
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (contact == null)
-            {
-                return NotFound();
-            }
-
-            return View(contact);
+            ModelState.AddModelError(nameof(contact.PhoneNumber), "The Phone number is already in use.");
         }
-
-        // GET: Contacts/Create
-        public IActionResult Create()
+        if (_context.Contact.Any(i => i.Email == contact.Email))
         {
-            return View();
+            ModelState.AddModelError(nameof(contact.Email), "The Email is already in use.");
         }
-
-        // POST: Contacts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,ShortName,Email,PhoneNumber")] Contact contact)
+        if (contact.Name == contact.ShortName)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(contact);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(contact);
+            ModelState.AddModelError(nameof(contact.ShortName), "Short name can't be the same as Name.");
         }
-
-        // GET: Contacts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        if (ModelState.IsValid)
         {
-            if (id == null || _context.Contact == null)
-            {
-                return NotFound();
-            }
-
-            var contact = await _context.Contact.FindAsync(id);
-            if (contact == null)
-            {
-                return NotFound();
-            }
-            return View(contact);
-        }
-
-        // POST: Contacts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ShortName,Email,PhoneNumber")] Contact contact)
-        {
-            if (id != contact.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(contact);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ContactExists(contact.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(contact);
-        }
-
-        // GET: Contacts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Contact == null)
-            {
-                return NotFound();
-            }
-
-            var contact = await _context.Contact
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (contact == null)
-            {
-                return NotFound();
-            }
-
-            return View(contact);
-        }
-
-        // POST: Contacts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Contact == null)
-            {
-                return Problem("Entity set 'ModelStateErrorContext.Contact'  is null.");
-            }
-            var contact = await _context.Contact.FindAsync(id);
-            if (contact != null)
-            {
-                _context.Contact.Remove(contact);
-            }
-            
+            _context.Add(contact);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View(contact);
+    }
+    // </snippet_4>
 
-        private bool ContactExists(int id)
+    // GET: Contacts/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || _context.Contact == null)
         {
-          return (_context.Contact?.Any(e => e.Id == id)).GetValueOrDefault();
+            return NotFound();
         }
+
+        var contact = await _context.Contact.FindAsync(id);
+        if (contact == null)
+        {
+            return NotFound();
+        }
+        return View(contact);
+    }
+
+    // POST: Contacts/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    // <snippet_1>
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ShortName,Email,PhoneNumber")] Contact contact)
+    {
+        if (id != contact.Id)
+        {
+            return NotFound();
+        }
+
+        // Attach Validation Error Message to the Model on validation failure.
+
+        if (_context.Contact.Any(i => i.PhoneNumber == contact.PhoneNumber))
+        {
+            ModelState.AddModelError(nameof(contact.PhoneNumber), "The Phone number is already in use.");
+        }
+        if (_context.Contact.Any(i => i.Email == contact.Email))
+        {
+            ModelState.AddModelError(nameof(contact.Email), "The Email is already in use.");
+        }
+        if (contact.Name == contact.ShortName)
+        {
+            ModelState.AddModelError(nameof(contact.ShortName), "Short name can't be the same as Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(contact);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactExists(contact.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(contact);
+    }
+    // </snippet_1>
+
+    // GET: Contacts/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null || _context.Contact == null)
+        {
+            return NotFound();
+        }
+
+        var contact = await _context.Contact
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (contact == null)
+        {
+            return NotFound();
+        }
+
+        return View(contact);
+    }
+
+    // <snippet_2>
+    // POST: Contacts/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        if (_context.Contact == null)
+        {
+            return Problem("Entity set 'ModelStateErrorContext.Contact'  is null.");
+        }
+        var contact = await _context.Contact.FindAsync(id);
+        if (contact != null)
+        {
+            _context.Contact.Remove(contact);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+    // </snippet_2>
+
+
+    private bool ContactExists(int id)
+    {
+        return (_context.Contact?.Any(e => e.Id == id)).GetValueOrDefault();
     }
 }
--- mvc/models/ModelStateError/Models/Contact.cs
+++ mvc/models/ModelStateError/Models/Contact.cs
@@ -3,7 +3,6 @@ namespace ModelStateError.Models;
 public class Contact
 {
     public int Id { get; set; }
-
     public string Name { get; set; } = String.Empty;
     public string ShortName { get; set; } = String.Empty;
     public string Email { get; set; } = String.Empty;
