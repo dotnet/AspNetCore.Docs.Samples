930a0060b76b026a2098895f1cd6ff21b2acaa99
Fiyaz Bin Hasan <fiyazhasan@live.com>
2023-03-16 08:49:45 +0600

    Basic JSON APIs sample

---
+++ fundamentals/minimal-apis/samples/json-apis/Models/WeatherForecast.cs
@@ -0,0 +1,16 @@
+﻿namespace JSON.Models;
+
+public class WeatherForecast {
+    public int  Id { get; set; }
+    public DateTime Date { get; set; }
+    public double TemperatureC { get; set; }
+    public string? Summary { get; set; }
+}
+
+public class WeatherForecastModel
+{
+        
+    public DateTime Date { get; set; }
+    public string? Summary { get; set; }
+    public IDictionary<string, double>? Temperatures { get; set; }
+}
