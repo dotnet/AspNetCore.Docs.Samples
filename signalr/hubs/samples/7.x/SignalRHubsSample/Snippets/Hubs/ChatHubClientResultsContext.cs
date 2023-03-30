0c001833a0f27f4af9d040d0ebe20bb2d7a650a8
Wade Pickett <wpickett@microsoft.com>
2023-03-30 14:11:30 -0700

    Adding 2 client result hub classes

---
+++ signalr/hubs/samples/7.x/SignalRHubsSample/Snippets/Hubs/ChatHubClientResultsContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SignalRHubsSample.Hubs;
+// <snippet_HubClientReturnContext>
+public class ChatHubClientResultsContext : Hub
+{
+    async Task SomeMethod(IHubContext<ChatHubClientResultsContext> context)
+    {
+        string result = await context.Clients.Client(connectionID).InvokeAsync<string>(
+            "GetMessage");
+    }
+    // </snippet_HubClientReturnContext>
+}
---
+++ signalr/hubs/samples/7.x/SignalRHubsSample/Snippets/Hubs/ChatHubClientResultsStronglyTyped.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SignalRHubsSample.Hubs;
+// <snippet_HubClientResultsStronglyTyped>
+public interface IClient
+{
+    Task<string> GetMessage();
+}
+
+public class ChatHubClientResultsStronglyTyped : Hub
+{
+    public interface IClient
+    {
+        Task<string> GetMessage();
+    }
+
+    public class ChatHub : Hub<IClient>
+    {
+        public async Task<string> WaitForMessage(string connectionId)
+        {
+            string message = await Clients.Client(connectionId).GetMessage();
+            return message;
+        }
+    }
+}
+// </snippet_HubClientResultsStronglyTyped>
