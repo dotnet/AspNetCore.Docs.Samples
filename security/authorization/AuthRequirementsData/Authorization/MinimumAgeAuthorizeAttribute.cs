9b45bd0b0d73cd23e22854d074c77a01b658cdc6
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-05-26 17:05:48 -1000

    IauthRequirementData

---
+++ security/authorization/AuthRequirementsData/Authorization/MinimumAgeAuthorizeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AuthRequirementsData.Authorization;
+
+class MinimumAgeAuthorizeAttribute : AuthorizeAttribute, IAuthorizationRequirement,
+                                     IAuthorizationRequirementData
+{
+    public MinimumAgeAuthorizeAttribute(int age) => Age = age;
+    public int Age { get; }
+
+    public IEnumerable<IAuthorizationRequirement> GetRequirements()
+    {
+        yield return this;
+    }
+}
