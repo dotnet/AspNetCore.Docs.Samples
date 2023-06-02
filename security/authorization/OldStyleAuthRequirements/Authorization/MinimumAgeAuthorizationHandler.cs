4e8176d4e29387183df2dff396cc2c5c6fe57cd0
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-06-02 13:35:11 -1000

    Update MinimumAgeAuthorizationHandler.cs

--- security/authorization/OldStyleAuthRequirements/Authorization/MinimumAgeAuthorizationHandler.cs
+++ security/authorization/OldStyleAuthRequirements/Authorization/MinimumAgeAuthorizationHandler.cs
@@ -4,7 +4,6 @@ using System.Security.Claims;
 
 namespace AuthRequirementsData.Authorization;
 
-
 class MinimumAgeAuthorizationHandler : AuthorizationHandler<MinimumAgeRequirement>
 {
     private readonly ILogger<MinimumAgeAuthorizationHandler> _logger;
