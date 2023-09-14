4a6432f7119a7106c0f89ea10c11e92d3fc51406
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-09-13 15:57:36 -1000

    keyed svc (#183)
    * keyed svc
    
    * Delete samples/WebKeyedService/appsettings.Development.json

---
+++ samples/WebKeyedService/CustomServicesApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebKeyedService;
+
+[ApiController]
+[Route("/cache")]
+public class CustomServicesApiController : Controller
+{
+    // GET /cache/big-cache
+    [HttpGet("big-cache")]
+    public ActionResult<object> GetOk([FromKeyedServices("big")]
+                                                    ICache cache)
+    {
+        var data = cache.Get("data-mvc");
+        return cache.Get("data-mvc");
+    }
+}
