5c21ac9c72d87255576fac946922e8b1a8918897
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-09-27 16:48:22 -1000

    r5 sample

---
+++ fundamentals/aot/diagnostics/Rdg5/Program.cs
@@ -0,0 +1,67 @@
+#define RDG5F //   RDG5 RDG5F
+#if NEVER
+#elif RDG5
+// <snippet_1>
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0,
+                                       AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+app.MapPut("/v1/todos/{id}",
+    ([AsParameters] TodoRequest todoRequest) => Results.Ok(todoRequest.Todo));
+
+app.Run();
+
+abstract class TodoRequest
+{
+    public int Id { get; set; }
+    public Todo? Todo { get; set; }
+}
+
+record Todo(int Id, string Task);
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
+// </snippet_1>
+#elif RDG5F
+// <snippet_1f>
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0,
+                                 AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+app.MapPut("/v1/todos/{id}", ([AsParameters] TodoRequest todoRequest) => Results.Ok(todoRequest.Todo));
+
+app.Run();
+
+class TodoRequest
+{
+    public int Id { get; set; }
+    public Todo? Todo { get; set; }
+}
+
+record Todo(int Id, string Task);
+
+[JsonSerializable(typeof(Todo[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
+// </snippet_1f>
+#endif
