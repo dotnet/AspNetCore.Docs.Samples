00d60c0740f680185f48c987e43b35875277d774
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-10-09 15:10:18 -1000

    fix

--- fundamentals/aot/diagnostics/Rdg8/Program.cs
+++ fundamentals/aot/diagnostics/Rdg8/Program.cs
@@ -1,4 +1,4 @@
-#define RDG008 //RDG008F
+#define RDG008F //RDG008F
 #if NEVER
 #elif RDG008
 // <snippet_1>
@@ -48,5 +48,42 @@ internal partial class AppJsonSerializerContext : JsonSerializerContext
 // </snippet_1>
 #elif RDG008F
 // <snippet_1f>
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0,
+                                 AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+app.MapPut("/v1/todos/{id}", ([AsParameters] TodoRequest request)
+    => Results.Ok(request.Id));
+
+app.Run();
+
+public class TodoRequest
+{
+    public DateTime DueDate { get; }
+    public int Id { get; private set; }
+    public string Task { get; private set; }
+
+    public TodoRequest(int Id, string Task, DateTime DueDate)
+
+    {
+        this.Id = Id;
+        this.Task = Task;
+        this.DueDate = DueDate;
+    }
+}
+
+[JsonSerializable(typeof(TodoRequest[]))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
 // </snippet_1f>
 #endif
