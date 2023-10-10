4b9d9f117b1ad7c0a70843b4f021797e3cd92ea5
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-10-10 03:05:07 +0100

    sample for rdg8

--- fundamentals/aot/diagnostics/Rdg8/Program.cs
+++ fundamentals/aot/diagnostics/Rdg8/Program.cs
@@ -4,86 +4,128 @@
 // <snippet_1>
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateSlimBuilder(args);
-
+var builder = WebApplication.CreateSlimBuilder();
+var todos = new[]
+{
+    new Todo(1, "Write tests", DateTime.UtcNow.AddDays(2)),
+    new Todo(2, "Fix tests",DateTime.UtcNow.AddDays(1))
+};
+builder.Services.AddSingleton(todos);
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0,
-                                 AppJsonSerializerContext.Default);
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
 
 var app = builder.Build();
 
-app.MapPut("/v1/todos/{id}", ([AsParameters] TodoRequest request)
-    => Results.Ok(request.Id));
+app.MapGet("/v1/todos/{id}", ([AsParameters] TodoItemRequest request) =>
+{
+    return request.Todos.ToList().Find(todoItem => todoItem.Id == request.Id)
+is Todo todo
+    ? Results.Ok(todo)
+    : Results.NotFound();
+});
 
 app.Run();
 
-public class TodoRequest
+public class TodoItemRequest
+{
+    public int Id { get; set; }
+    public Todo[] Todos { get; set; }
+
+    public TodoItemRequest(int id, Todo[] todos)
+    {
+        Id = id;
+        Todos = todos;
+    }
+    
+    // Additional Constructor
+    public TodoItemRequest()
+    {
+        Id = 1;
+        Todos = [new Todo(1, "Write tests", DateTime.UtcNow.AddDays(2))];
+    }
+
+}
+
+public class Todo
 {
     public DateTime DueDate { get; }
     public int Id { get; private set; }
     public string Task { get; private set; }
 
-    // Additional constructors
-    public TodoRequest(int Id, string Task, DateTime DueDate)
+    public Todo(int id, string task, DateTime dueDate)
 
     {
-        this.Id = Id;
-        this.Task = Task;
-        this.DueDate = DueDate;
-    }
-
-    public TodoRequest(int Id, string Task)
-        : this(Id, Task, default)
-    {
+        Id = id;
+        Task = task;
+        DueDate = dueDate;
     }
 }
 
-[JsonSerializable(typeof(TodoRequest[]))]
+[JsonSerializable(typeof(Todo[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
-
 }
 // </snippet_1>
 #elif RDG008F
 // <snippet_1f>
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateSlimBuilder(args);
-
+var builder = WebApplication.CreateSlimBuilder();
+var todos = new[]
+{
+    new Todo(1, "Write tests", DateTime.UtcNow.AddDays(2)),
+    new Todo(2, "Fix tests",DateTime.UtcNow.AddDays(1))
+};
+builder.Services.AddSingleton(todos);
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0,
-                                 AppJsonSerializerContext.Default);
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
 });
 
 var app = builder.Build();
 
-app.MapPut("/v1/todos/{id}", ([AsParameters] TodoRequest request)
-    => Results.Ok(request.Id));
+app.MapGet("/v1/todos/{id}", ([AsParameters] TodoItemRequest request) =>
+{
+    return request.Todos.ToList().Find(todoItem => todoItem.Id == request.Id)
+is Todo todo
+    ? Results.Ok(todo)
+    : Results.NotFound();
+});
 
 app.Run();
 
-public class TodoRequest
+public class TodoItemRequest
+{
+    public int Id { get; set; }
+    public Todo[] Todos { get; set; }
+
+    public TodoItemRequest(int id, Todo[] todos)
+    {
+        Id = id;
+        Todos = todos;
+    }
+}
+
+public class Todo
 {
     public DateTime DueDate { get; }
     public int Id { get; private set; }
     public string Task { get; private set; }
 
-    public TodoRequest(int Id, string Task, DateTime DueDate)
+    public Todo(int id, string task, DateTime dueDate)
 
     {
-        this.Id = Id;
-        this.Task = Task;
-        this.DueDate = DueDate;
+        Id = id;
+        Task = task;
+        DueDate = dueDate;
     }
 }
 
-[JsonSerializable(typeof(TodoRequest[]))]
+[JsonSerializable(typeof(Todo[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
-
 }
 // </snippet_1f>
 #endif
