725d1d168a0a668df2021e9b1b07db043309ccde
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2023-10-16 16:24:07 -1000

    Keyed service hub example

---
+++ signalr/hubs/KeyedSvsHub/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddKeyedSingleton<ICache, BigCache>("big");
+builder.Services.AddKeyedSingleton<ICache, SmallCache>("small");
+
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapRazorPages();
+app.MapHub<MyHub>("/myHub");
+
+app.Run();
+
+public interface ICache
+{
+    object Get(string key);
+}
+public class BigCache : ICache
+{
+    public object Get(string key) => $"Resolving {key} from big cache.";
+}
+
+public class SmallCache : ICache
+{
+    public object Get(string key) => $"Resolving {key} from small cache.";
+}
+
+public class MyHub : Hub
+{
+    public void Method([FromKeyedServices("small")] ICache cache)
+    {
+        Console.WriteLine(cache.Get("signalr"));
+    }
+}
