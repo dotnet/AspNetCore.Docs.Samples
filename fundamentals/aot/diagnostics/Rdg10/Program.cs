42640a546e48565db7d1049a25035dd8a377d22c
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-01-10 15:44:13 -1000

    test

--- fundamentals/aot/diagnostics/Rdg10/Program.cs
+++ fundamentals/aot/diagnostics/Rdg10/Program.cs
@@ -1,64 +1,31 @@
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateSlimBuilder();
-var todos = new[]
-{
-    new Todo(1, "Write tests"),
-    new Todo(2, "Fix tests")
-};
-
-builder.Services.AddSingleton(todos);
+var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0,
+                                       AppJsonSerializerContext.Default);
 });
 
 var app = builder.Build();
 
-
-app.MapGet("/v1/todos/{id?}", (int? Id, Todo[] todos) =>
-{
-    if (Id.HasValue)
-    {
-        return todos.ToList().Find(todoItem => todoItem.Id == Id)
-   is Todo todo
-       ? Results.Ok(todo)
-       : Results.NotFound();
-    }
-    else
-    {
-        return Results.Ok(todos);
-    }
-
-});
+app.MapPut("/v1/todos/{id}",
+           ([AsParameters] TodoRequest todoRequest) => Results.Ok(todoRequest.Todo));
 
 app.Run();
 
-
-public class Todo
+class TodoRequest(int id, Todo todo)
 {
-    public DateTime DueDate { get; }
-    public int Id { get; private set; }
-    public string Task { get; private set; }
-
-    // Additional constructors
-    public Todo(int Id, string Task, DateTime DueDate)
-
-    {
-        this.Id = Id;
-        this.Task = Task;
-        this.DueDate = DueDate;
-    }
-
-    public Todo(int Id, string Task)
-        : this(Id, Task, default)
-    {
-    }
+    public int Id { get; set; } = id;
+    public Todo? Todo { get; set; } = todo;
 }
+
+
+record Todo(int Id, string Task);
 
 [JsonSerializable(typeof(Todo[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
-}
 
+}
