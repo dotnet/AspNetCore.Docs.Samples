624a6aa39af47b3a7c4db35576f8383b81975b13
wadepickett <wpickett@microsoft.com>
2024-01-17 16:05:08 -0800

    Moving IClient interface in ChatHubClientResultsStrongyTyped.cs

--- signalr/hubs/samples/7.x/SignalRHubsSample/Snippets/Hubs/ChatHubClientResultsStronglyTyped.cs
+++ signalr/hubs/samples/7.x/SignalRHubsSample/Snippets/Hubs/ChatHubClientResultsStronglyTyped.cs
@@ -3,13 +3,13 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SignalRHubsSample.Hubs;
 // <snippet_HubClientResultsStronglyTyped>
+public interface IClient
+{
+    Task<string> GetMessage();
+}
+
 public class ChatHubClientResultsStronglyTyped : Hub
 {
-    public interface IClient
-    {
-        Task<string> GetMessage();
-    }
-
     public class ChatHub : Hub<IClient>
     {
         public async Task<string> WaitForMessage(string connectionId)
