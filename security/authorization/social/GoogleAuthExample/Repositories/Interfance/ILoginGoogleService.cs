561a4554fc93b3a97987c3c7f82a13697e0b7f6e
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2024-10-12 14:40:20 +0300

    update

---
+++ security/authorization/social/GoogleAuthExample/Repositories/Interfance/ILoginGoogleService.cs
@@ -0,0 +1,9 @@
+﻿namespace GoogleAuthticationExample.Repositories.Interfance
+{
+    public interface ILoginGoogleService
+    {
+        Task<bool> LoginExtnal(string email);
+        Task<bool> RegisterExtnal(string email);
+
+    }
+}
