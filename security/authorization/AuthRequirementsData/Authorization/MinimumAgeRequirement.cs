d10e581d90cafc4e7fe7bd9e232fc43384e79c8d
guardrex <1622880+guardrex@users.noreply.github.com>
2025-05-16 08:17:51 -0400

    Update AuthRequirmentsData sample app

--- security/authorization/AuthRequirementsData/Authorization/MinimumAgeRequirement.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Authorization;
-
-namespace AuthRequirementsData.Authorization;
-
-class MinimumAgeRequirement(int age) : IAuthorizationRequirement
-{
-    public int Age { get; private set; } = age;
-}
