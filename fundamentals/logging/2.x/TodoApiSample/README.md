dec8752edfda1a13b2797fb30a5a5c1b69c4767c
guardrex <1622880+guardrex@users.noreply.github.com>
2025-09-18 09:53:46 -0400

    Add logging sample apps

---
+++ fundamentals/logging/2.x/TodoApiSample/README.md
@@ -0,0 +1,3 @@
+# ASP.NET Core logging sample application
+
+This sample application is for the [ASP.NET Core Logging](https://learn.microsoft.com/aspnet/core/fundamentals/logging/index) article and is based on the sample created for the [First Web API with ASP.NET Core MVC](https://learn.microsoft.com/aspnet/core/tutorials/first-web-api) tutorial.
---
+++ fundamentals/logging/3.x/LoggingConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+#region snippet_LoggerFactory
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                .AddConsole()
+                .AddEventLog();
+        });
+        ILogger logger = loggerFactory.CreateLogger<Program>();
+        logger.LogInformation("Example log message");
+    }
+}
+#endregion
