9adc6a9bf31b9c35665d6154d7615e5353e77d2b
Luke Latham <1622880+guardrex@users.noreply.github.com>
2026-04-07 20:26:13 -0400

    Remove BlazorWebAppRolesWithIdentity sample app (#302)

--- security/authorization/BlazorWebAppRolesWithIdentity/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace BlazorWebAppRolesWithIdentity.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
