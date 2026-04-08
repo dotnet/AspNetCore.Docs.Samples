bf78ea3fc902a56f2355df6c31e025b89f7fc8fd
guardrex <1622880+guardrex@users.noreply.github.com>
2026-04-07 20:25:12 -0400

    Remove BlazorWebAppRolesWithIdentity sample app

--- security/authorization/BlazorWebAppRolesWithIdentity/Components/Account/PasskeyInputModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BlazorWebAppRolesWithIdentity.Components.Account
-{
-    public class PasskeyInputModel
-    {
-        public string? CredentialJson { get; set; }
-        public string? Error { get; set; }
-    }
-}
--- security/authorization/BlazorWebAppRolesWithIdentity/Components/Account/PasskeyOperation.cs
+++
@@ -1,8 +0,0 @@
-namespace BlazorWebAppRolesWithIdentity.Components.Account
-{
-    public enum PasskeyOperation
-    {
-        Create = 0,
-        Request = 1,
-    }
-}
